f04099e3fb9b7efdeaf7d0e65c121e67b61ddb53
LordAldi <aldianugra09@gmail.com>
2022-04-24 01:13:00 +0700

    create postlist, postitem

---
+++ src/components/PostList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="post-list">
+    <div class="post" v-for="post in posts" :key="post.id">
+      <PostItem :post="post" />
+    </div>
+  </div>
+</template>
+<script>
+import PostItem from './PostItem.vue'
+export default {
+  props: {
+    posts: {
+      required: true,
+      type: Array
+    }
+  },
+
+  components: { PostItem }
+}
+</script>
+<style></style>
